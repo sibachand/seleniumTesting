7ee2158ca881c1b3d59a3a39265fe4be7bdb6c05
Siba Chand <Siba Chand@192.168.2.10>
2018-09-13 22:04:51 +0530

    2nd change

--- jenkinsProject/src/jenkinsPackage/test_Script.java
+++ jenkinsProject/src/jenkinsPackage/test_Script.java
@@ -3,5 +3,7 @@ package jenkinsPackage;
 public class test_Script {
 public static void main(String[] args) {
 	System.out.println("Jenkins project");
-}
+	System.out.println("2nd change");
+	System.out.println("3rd change");
+	}
 }
